763d5efb88b7d2956b12e5bcd8b3c6f28a86c70a
Molcarrus <lenovolaptop.1202023@gmail.com>
2025-06-12 21:56:37 +0530

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::{prelude::*, render::mesh::PlaneMeshBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup (
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(
+            PlaneMeshBuilder::from_length(20.0)
+        )),
+        MeshMaterial3d( materials.add(
+            Color::srgb(0.0, 1.0, 0.3)
+        )),
+        Transform::from_xyz(0.0, -0.5, 0.0)
+    ));
+
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(20.0, 20.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y)
+    ));
 }
