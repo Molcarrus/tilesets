3c607b7254ed357b9a9beb14241797b8fea6a663
Molcarrus <lenovolaptop.1202023@gmail.com>
2025-07-04 18:49:21 +0530

    render

--- src/ui.rs
+++ src/ui.rs
@@ -7,32 +7,38 @@ use crate::builder::{
     generate_tileset
 };
 
-use crate::draw::draw;
+#[derive(Event)]
+pub struct ValueChangeEvent;
+
+#[derive(Component)]
+pub struct Building;
 
 pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Environment>()
+        app.add_event::<ValueChangeEvent>()
+            .init_resource::<Environment>()
             .add_systems(EguiContextPass, ui_system);
     }
 }
 
 fn ui_system(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     mut values: ResMut<Environment>,
-    mut contexts: EguiContexts
+    mut contexts: EguiContexts,
+    mut value_changed_event: EventWriter<ValueChangeEvent>
 ) {
     egui::Window::new("Environment values")
         .show(contexts.ctx_mut(), |ui| {
         ui.horizontal(|ui| {
             ui.label("Size(rows and columns): ");
-            ui.add(Slider::new(
+            if ui.add(Slider::new(
                     &mut values.size, 
                     2..=50
-            ))
+            )).changed() {
+                value_changed_event.write(ValueChangeEvent);
+            }
+            
         });
 
         ui.horizontal(|ui| {
@@ -45,9 +51,64 @@ fn ui_system(
                 .add(egui::Button::new("Generate"))
                 .clicked()
             {
-                values.tileset = generate_tileset(values.size);
-                draw(commands, meshes, materials, values.size, values.tileset.clone());
+                value_changed_event.write(ValueChangeEvent);
             }
         })
     });
+}
+
+fn render_buildings(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<Entity, With<Building>>,
+    mut values: ResMut<Environment>,
+    mut value_events: EventReader<ValueChangeEvent>
+) {
+    for _ in value_events.read() {
+        for entity in query.iter() {
+            commands.entity(entity).despawn();
+        }
+
+        let coords = get_coords(values.size);
+        values.tileset = generate_tileset(values.size);
+
+        for i in 0..values.size {
+            for j in 0..values.size {
+                let entity_id = commands.spawn((
+                    Mesh3d(meshes.add(
+                        Cuboid::new(
+                            0.0, values.tileset[i as usize][j as usize] as f32, 1.0
+                        )
+                    )),
+                    MeshMaterial3d(materials.add(
+                        Color::srgb(0.128, 0.128, 0.128)
+                    )),
+                    Transform::from_translation(coords[i as usize][j as usize])
+                )).id();
+            }
+        }
+    }
+}
+
+pub fn get_coords(size: u8) -> Vec<Vec<Vec3>> {
+    let mut coords: Vec<Vec<Vec3>> = vec![vec![Vec3::ZERO; size as usize]; size as usize];
+
+    let x_start = - 1.0 * ((3.0 * (size - 1) as f32) / 4.0);
+
+    let mut x_coord = x_start;
+
+    for i in 0..size as usize {
+        let mut z_coord = x_start.abs();
+        for j in 0..size as usize {
+            coords[i][j].x = x_coord;
+            coords[i][j].z = z_coord;
+
+            x_coord += 1.5;
+        }
+        x_coord = x_start;
+        z_coord = z_coord - 1.5;
+    }
+
+    coords
 }
